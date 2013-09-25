6e8a160089a7e2c995c36e11749d8d212f7aa381
fspjorge <fspjorge@gmail.com>
2013-09-25 09:34:59 +0100

    implementação dos histogramas. rever função 5.

--- src/face.cpp
+++ src/face.cpp
@@ -8,10 +8,49 @@
 #include <iostream>     // std::cout
 #include <algorithm>    // std::min
 #include <cmath>        // std::abs
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
+// http://stackoverflow.com/questions/15771512/compare-histograms-of-grayscale-images-in-opencv
+void show_grayscale_histogram(std::string const& name, cv::Mat1b const& image)
+{
+    // Set histogram bins count
+    int bins = 256;
+    int histSize[] = {bins};
+
+    // Set ranges for histogram bins
+    float lranges[] = {0, 256};
+    const float* ranges[] = {lranges};
+
+    // create matrix for histogram
+    cv::Mat hist;
+    int channels[] = {0};
+
+    // create matrix for histogram visualization
+    int const hist_height = 256;
+    cv::Mat3b hist_image = cv::Mat3b::zeros(hist_height, bins);
+
+    cv::calcHist(&image, 1, channels, cv::Mat(), hist, 1, histSize, ranges, true, false);
+
+    double max_val=0;
+    minMaxLoc(hist, 0, &max_val);
+
+    // visualize each bin
+    for(int b = 0; b < bins; b++) {
+        float const binVal = hist.at<float>(b);
+        int   const height = cvRound(binVal*hist_height/max_val);
+        cv::line
+            ( hist_image
+            , cv::Point(b, hist_height-height), cv::Point(b, hist_height)
+            , cv::Scalar::all(255)
+            );
+    }
+    //cv::imshow(name, hist_image);
+}
 
 static void error(const char* s1, const char* s2)
 {
@@ -19,24 +58,21 @@ static void error(const char* s1, const char* s2)
     exit(1);
 }
 
-double distanceCalculate(double x1, double y1, double x2, double y2) //
+Point midpoint(double x1, double y1, double x2, double y2)
 {
-    double x = x1 - x2;
-    double y = y1 - y2;
-    double dist;
+	double newX = (x1 + x2) / 2;
+	double newY = (y1 + y2) / 2;
 
-    dist = pow(x,2)+pow(y,2);           //calculating distance by euclidean formula
-    dist = sqrt(dist);                  //sqrt is function in math.h
-
-    return dist;
+	return Point(newX, newY);
 }
+
 
 int main()
 {
     if (!stasm_init("data", 0 /*trace*/))
         error("stasm_init failed: ", stasm_lasterr());
 
-    static const char* path = "data/MS_480.jpg";
+    static const char* path = "data/sofiaporto_tipopasse.png";
 
     cv::Mat_<unsigned char> img(cv::imread(path, CV_LOAD_IMAGE_GRAYSCALE));
 
@@ -68,29 +104,12 @@ int main()
 //        	img(cvRound(landmarks[38*2+1]), cvRound(landmarks[38*2])) = 255;
 //      		img(cvRound(landmarks[39*2+1]), cvRound(landmarks[39*2])) = 255;
 
-        Point LPupil;
-        LPupil.y = cvRound(landmarks[38*2+1]);
-        LPupil.x = cvRound(landmarks[38*2]);
-
-        Point RPupil;
-        RPupil.y = cvRound(landmarks[39*2+1]);
-        RPupil.x = cvRound(landmarks[39*2]);
-
-        Point CNoseTip;
-        CNoseTip.y = cvRound(landmarks[52*2+1]);
-        CNoseTip.x = cvRound(landmarks[52*2]);
-
-        Point LEyebrowInner;
-        LEyebrowInner.y = cvRound(landmarks[21*2+1]);
-        LEyebrowInner.x = cvRound(landmarks[21*2]);
-
-        Point CNoseBase;
-        CNoseBase.y = cvRound(landmarks[56*2+1]);
-        CNoseBase.x = cvRound(landmarks[56*2]);
-
-        Point CTipOfChin;
-        CTipOfChin.y = cvRound(landmarks[6*2+1]);
-        CTipOfChin.x = cvRound(landmarks[6*2]);
+        Point LPupil = Point(cvRound(landmarks[38*2]), cvRound(landmarks[38*2+1]));
+        Point RPupil = Point(cvRound(landmarks[39*2]), cvRound(landmarks[39*2+1]));
+        Point CNoseTip = Point(cvRound(landmarks[52*2]), cvRound(landmarks[52*2+1]));
+        Point LEyebrowInner = Point(cvRound(landmarks[21*2]), cvRound(landmarks[21*2+1]));
+        Point CNoseBase = Point(cvRound(landmarks[56*2]), cvRound(landmarks[56*2+1]));
+        Point CTipOfChin = Point(cvRound(landmarks[6*2]), cvRound(landmarks[6*2+1]));
 
         // draw a line between the two eyes
         line(img, RPupil, LPupil, cvScalar(255,0,255), 1);
@@ -138,19 +157,131 @@ int main()
         double sp = alpha*(1-roll) + beta*(1-yaw) + gamma*(1-pitch);
         printf("sp = %f\n######################\n", sp);
 
+        // SI
+        // SI = 1 - F(std(mc))
+
+        // Finding the 8 points
+        Point p1 = midpoint((double)cvRound(landmarks[0*2]), (double)cvRound(landmarks[0*2+1]), (double)cvRound(landmarks[58*2]), (double)cvRound(landmarks[58*2+1]));
+        Point p2 = midpoint((double)cvRound(landmarks[3*2]), (double)cvRound(landmarks[3*2+1]), (double)cvRound(landmarks[58*2]), (double)cvRound(landmarks[58*2+1]));
+        Point p3 = LEyebrowInner;
+        Point p4 = midpoint((double)LEyebrowInner.x, (double)LEyebrowInner.y, (double)CNoseTip.x, (double)CNoseTip.y);
+        Point p5 = CNoseTip;
+        Point p6 = CTipOfChin;
+        Point p7 = midpoint((double)cvRound(landmarks[54*2]), (double)cvRound(landmarks[54*2+1]), (double)cvRound(landmarks[12*2]), (double)cvRound(landmarks[12*2+1]));
+        Point p8 = midpoint((double)cvRound(landmarks[54*2]), (double)cvRound(landmarks[54*2+1]), (double)cvRound(landmarks[9*2]), (double)cvRound(landmarks[9*2+1]));
+
+        // variancia dos 8 pontos através de sigmoid
+        // For each such reference point, we select the corresponding region w of the image, whose size is proportional to the square containing the whole face.
+        // obter os pontos de uma sub-área da imagem cujo centro de massa é cada um dos 8 pontos
+
+        // http://stackoverflow.com/questions/12369697/access-sub-matrix-of-a-multidimensional-mat-in-opencv
+        // Parameters:
+        // x – x-coordinate of the top-left corner.
+        // y – y-coordinate of the top-left corner (sometimes bottom-left corner).
+        // width – width of the rectangle.
+        // height – height of the rectangle.
+
+        Mat subMatPt1 = img(cv::Rect(p1.x - (cvRound(img.cols*0.1)/2), p1.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt2 = img(cv::Rect(p2.x - (cvRound(img.cols*0.1)/2), p2.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt3 = img(cv::Rect(p3.x - (cvRound(img.cols*0.1)/2), p3.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt4 = img(cv::Rect(p4.x - (cvRound(img.cols*0.1)/2), p4.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt5 = img(cv::Rect(p5.x - (cvRound(img.cols*0.1)/2), p5.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt6 = img(cv::Rect(p6.x - (cvRound(img.cols*0.1)/2), p6.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt7 = img(cv::Rect(p7.x - (cvRound(img.cols*0.1)/2), p7.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+        Mat subMatPt8 = img(cv::Rect(p8.x - (cvRound(img.cols*0.1)/2), p8.y - (cvRound(img.rows*0.1)/2), cvRound(img.cols*0.1), cvRound(img.rows*0.1)));
+
+        rectangle( img,
+                   Point( p1.x - cvRound(img.cols*0.1/2), p1.y - (cvRound(img.rows*0.1/2))),
+                   Point( p1.x + cvRound(img.cols*0.1/2), p1.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        rectangle( img,
+                   Point( p2.x - cvRound(img.cols*0.1/2), p2.y - (cvRound(img.rows*0.1/2))),
+                   Point( p2.x + cvRound(img.cols*0.1/2), p2.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        rectangle( img,
+                   Point( p3.x - cvRound(img.cols*0.1/2), p3.y - (cvRound(img.rows*0.1/2))),
+                   Point( p3.x + cvRound(img.cols*0.1/2), p3.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        rectangle( img,
+                   Point( p4.x - cvRound(img.cols*0.1/2), p4.y - (cvRound(img.rows*0.1/2))),
+                   Point( p4.x + cvRound(img.cols*0.1/2), p4.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        rectangle( img,
+                   Point( p5.x - cvRound(img.cols*0.1/2), p5.y - (cvRound(img.rows*0.1/2))),
+                   Point( p5.x + cvRound(img.cols*0.1/2), p5.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+        
+        rectangle( img,
+                   Point( p6.x - cvRound(img.cols*0.1/2), p6.y - (cvRound(img.rows*0.1/2))),
+                   Point( p6.x + cvRound(img.cols*0.1/2), p6.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        rectangle( img,
+                   Point( p7.x - cvRound(img.cols*0.1/2), p7.y - (cvRound(img.rows*0.1/2))),
+                   Point( p7.x + cvRound(img.cols*0.1/2), p7.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        rectangle( img,
+                   Point( p8.x - cvRound(img.cols*0.1/2), p8.y - (cvRound(img.rows*0.1/2))),
+                   Point( p8.x + cvRound(img.cols*0.1/2), p8.y + cvRound(img.rows*0.1/2)),
+                   Scalar( 0, 255, 255 ),
+                   1,
+                   1 );
+
+        cv::imwrite("histograms/1.png",subMatPt1); // save
+        cv::imwrite("histograms/2.png",subMatPt2); // save
+        cv::imwrite("histograms/3.png",subMatPt3); // save
+        cv::imwrite("histograms/4.png",subMatPt4); // save
+        cv::imwrite("histograms/5.png",subMatPt5); // save
+        cv::imwrite("histograms/6.png",subMatPt6); // save
+        cv::imwrite("histograms/7.png",subMatPt7); // save
+        cv::imwrite("histograms/8.png",subMatPt8); // save
+
+        // histograma da área
+        // ver http://docs.opencv.org/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html
+        show_grayscale_histogram("w1 hist", subMatPt1);
+        show_grayscale_histogram("w2 hist", subMatPt2);
+        show_grayscale_histogram("w3 hist", subMatPt3);
+        show_grayscale_histogram("w4 hist", subMatPt4);
+        show_grayscale_histogram("w5 hist", subMatPt5);
+        show_grayscale_histogram("w6 hist", subMatPt6);
+        show_grayscale_histogram("w7 hist", subMatPt7);
+        show_grayscale_histogram("w8 hist", subMatPt8);
+
+        // comparar histogramas http://docs.opencv.org/doc/tutorials/imgproc/histograms/histogram_comparison/histogram_comparison.html
 
         nfaces++;
     }
 
-    // point coordinates according to stasm_landmarks.h
+    // Criar a matriz de origem e destino
+    Mat src; // a origem é cada uma das 8 anteriores
 
+    // separar a imagem
 
 
     printf("%s: %d face(s)\n", path, nfaces);
     fflush(stdout);
     cv::imwrite("minimal2.bmp", img);
-    cv::imshow("stasm minimal2", img);
-    cv::waitKey();
+    cv::imshow("stasm", img);
+    cv::waitKey(0);
 
     return 0;
 }
