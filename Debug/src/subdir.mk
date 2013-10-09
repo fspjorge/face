ba9655441574c6ffcac10b7d3b3aaad5c3389c7a
fspjorge <fspjorge@gmail.com>
2013-10-09 09:52:08 +0100

    - se a área da submatriz saltar fora da imagem dá erro porque os pixeis não existem. É preciso uma excepção... - falta implementar 4.d

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/face.cpp 
+../src/main.cpp 
 
 OBJS += \
-./src/face.o 
+./src/main.o 
 
 CPP_DEPS += \
-./src/face.d 
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/local/include/opencv -I/usr/local/include/gsl -I/home/jorge/stasm4.0.0/stasm/ -I/usr/local/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I/home/jorge/stasm4.0.0/stasm -I/usr/local/include -I/usr/local/include/gsl -I/usr/local/include/opencv -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- dissertacao/Debug/src/main.d
+++
@@ -1,3 +0,0 @@
-src/main.d: ../src/main.cpp /home/jorge/stasm4.0.0/stasm/stasm_lib.h
-
-/home/jorge/stasm4.0.0/stasm/stasm_lib.h:
